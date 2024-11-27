36443fd1d76b10bdfb7aa36252e8027a39990ba5
tunhnakca <tunahanakca67@gmail.com>
2024-11-28 00:45:59 +0300

    Parcel and spring boot configured

--- src/main/java/com/sau/learningplatform/Controller/HomeController.java
+++ src/main/java/com/sau/learningplatform/Controller/HomeController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/home")
-    public void homepage(){
-
-
+    public String homepage() {
+        return "layout";
     }
 }
