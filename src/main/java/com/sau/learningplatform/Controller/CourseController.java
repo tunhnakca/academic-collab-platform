753b59d6747df76a5c9bad02c97c55763f3dcb63
tunhnakca <tunahanakca67@gmail.com>
2024-12-03 00:32:51 +0300

    Thymeleaf dialect added

---
+++ src/main/java/com/sau/learningplatform/Controller/CourseController.java
@@ -0,0 +1,14 @@
+package com.sau.learningplatform.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class CourseController {
+    @GetMapping("/courses")
+    public String getCourses(Model model) {
+        // Add logic to fetch courses
+        return "courses";
+    }
+}
