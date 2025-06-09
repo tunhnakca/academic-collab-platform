c8f31299635ad721ed8f0fbfa5bc5176f3887ed0
mrtmnr <mertmunar@gmail.com>
2025-06-09 23:49:44 +0300

    save post method is written

--- src/main/java/com/sau/learningplatform/Service/PostService.java
+++ src/main/java/com/sau/learningplatform/Service/PostService.java
@@ -1,13 +1,24 @@
 package com.sau.learningplatform.Service;
 
 
+import com.sau.learningplatform.Entity.Post;
+import com.sau.learningplatform.Entity.Project;
+import com.sau.learningplatform.Entity.User;
+import com.sau.learningplatform.EntityResponse.MessageResponse;
+import com.sau.learningplatform.EntityResponse.MessageResponseWithStatus;
+import com.sau.learningplatform.EntityResponse.PostRequest;
 import com.sau.learningplatform.EntityResponse.PostResponse;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostService {
     List<PostResponse> getParentPostResponsesByProjectId(int id);
 
     void deleteById(int id);
 
+
+    Post findById(int parentPostId);
+
+    MessageResponseWithStatus savePostRequest(User user, Project project, PostRequest request);
 }
