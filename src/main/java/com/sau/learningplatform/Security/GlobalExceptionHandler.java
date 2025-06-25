14329ee417eceaab081a2b77d3787ea49d3b2b32
mrtmnr <mertmunar@gmail.com>
2025-06-25 15:26:17 +0300

    user list sorted and forgot-pass error pages set

--- src/main/java/com/sau/learningplatform/Security/GlobalExceptionHandler.java
+++ src/main/java/com/sau/learningplatform/Security/GlobalExceptionHandler.java
@@ -10,11 +10,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public String handleException(Exception e, Model model) {
+    public String handleException(Exception e) {
 
         log.warn("Error: " + e.getMessage());
 
-        model.addAttribute("errorMessage", "Something went wrong. Please try again later.");
 
         return "custom-error";
     }
